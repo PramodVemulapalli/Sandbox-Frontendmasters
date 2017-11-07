a5a25ec9391a9f5420c2e4c814c541cc556a7313
Vemulapalli <v.pramodkrishna@gmail.com>
2017-11-07 08:34:11 -0500

    added sign and create action

--- src/Application.js
+++ src/Application.js
@@ -9,14 +9,36 @@ import './Application.css';
 class Application extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentuser: null
+    };
+  }
+
+  componentDidMount(){
+    auth.onAuthStateChanged((currentuser) => {
+      console.log('AUTH_CHANGE',CurrentUser);
+      this.setState({ currentuser });
+    });
   }
 
   render() {
+
+    const { currentuser } = this.state;
+
     return (
       <div className="Application">
         <header className="Application--header">
           <h1>Lunch Rush</h1>
         </header>
+        <div>
+          { !currentuser && <SignIn /> }
+          { currentuser &&
+            <div>
+              <NewRestaurant />
+              <CurrentUser user={currentuser} />
+            </div>
+          }
+        </div>
       </div>
     );
   }
--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  storageBucket: '',
-  messagingSenderId: ''
+  apiKey: "AIzaSyAOU9XJOb9lwbl3nTY-Y5F7EzIcpDGyu0A",
+  authDomain: "homefood-d7c6d.firebaseapp.com",
+  databaseURL: "https://homefood-d7c6d.firebaseio.com",
+  projectId: "homefood-d7c6d",
+  storageBucket: "homefood-d7c6d.appspot.com",
+  messagingSenderId: "512397701929"
 };
 
 firebase.initializeApp(config);
